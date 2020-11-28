909ed04317a773fe7dbd5ec1e517046e65868e65
Stephen Turcotte <stephenturcotte@Lawsons-iMac.local>
2020-11-28 15:05:04 -0700

    Changed app.js

--- src/App.js
+++ src/App.js
@@ -56,7 +56,7 @@ function App() {
   return (
     <div className="App">
       <header>
-        <h1>Adventure Online</h1>
+        <h1>Adventures Online</h1>
         <SignOut />
       </header>
 
